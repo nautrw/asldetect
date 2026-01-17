15d8f2cb98f0c2c0fa690b36d921c135eb016649
nautrw <nautrw@proton.me>
2026-01-17 14:17:40 -0500

    add landmarking for single images

--- main.py
+++ main.py
@@ -7,32 +7,49 @@ model_path = "./models/hand_landmarker.task"
 
 capture = cv2.VideoCapture(0)
 
+def show_result(
+        result: vision.HandLandmarkerResult, output_image: mp.Image, timestamp_ms: int
+    ):
+        print(result)
 
-def print_result(
-    result: vision.HandLandmarkerResult, output_image: mp.Image, timestamp_ms: int
-):
-    print(f"landmarker result: {result}")
+def capture_live_steam():
+    options = vision.HandLandmarkerOptions(
+        base_options=mp.tasks.BaseOptions(model_asset_path=model_path),
+        running_mode=vision.RunningMode.LIVE_STREAM,
+        result_callback=show_result,
+    )
+    
+    with vision.HandLandmarker.create_from_options(options) as landmarker:
+        frame = 1
+    
+        while True:
+            success, img = capture.read()
+            img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            
+            mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=img_rgb)
+            landmarker.detect_async(mp_image, frame)
+    
+            frame += 1
+    
+            cv2.imshow("Landmarker", img)
+            cv2.waitKey(1)
 
+def capture_image():
+    options = vision.HandLandmarkerOptions(
+        base_options=mp.tasks.BaseOptions(model_asset_path=model_path),
+        running_mode=vision.RunningMode.IMAGE
+    )
 
-options = vision.HandLandmarkerOptions(
-    base_options=mp.tasks.BaseOptions(model_asset_path=model_path),
-    running_mode=vision.RunningMode.LIVE_STREAM,
-    result_callback=print_result,
-)
-
-with vision.HandLandmarker.create_from_options(options) as landmarker:
-    frame = 1
-
-    while True:
+    with vision.HandLandmarker.create_from_options(options) as landmarker:
         success, img = capture.read()
-
         img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
         mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=img_rgb)
+        result = landmarker.detect(mp_image)
 
-        landmarker.detect_async(mp_image, frame)
-
-        frame += 1
+        print(result)
 
         cv2.imshow("Landmarker", img)
-        cv2.waitKey(1)
+        cv2.waitKey(100000000) # Prevents the window from closing immediately
+
+capture_image()
