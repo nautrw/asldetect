2b3b9360772fbea27ee26685c35d4f87ee74b9bd
nautrw <nautrw@proton.me>
2026-01-22 00:02:00 -0500

    add data parser

--- main.py
+++ main.py
@@ -7,10 +7,13 @@ model_path = "./models/hand_landmarker.task"
 
 capture = cv2.VideoCapture(0)
 
+
+# show_result is only called for capture_live_steam()
 def show_result(
-        result: vision.HandLandmarkerResult, output_image: mp.Image, timestamp_ms: int
-    ):
-        print(result)
+    result: vision.HandLandmarkerResult, output_image: mp.Image, timestamp_ms: int
+):
+    print(result)
+
 
 def capture_live_steam():
     options = vision.HandLandmarkerOptions(
@@ -18,26 +21,27 @@ def capture_live_steam():
         running_mode=vision.RunningMode.LIVE_STREAM,
         result_callback=show_result,
     )
-    
+
     with vision.HandLandmarker.create_from_options(options) as landmarker:
         frame = 1
-    
+
         while True:
             success, img = capture.read()
             img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-            
+
             mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=img_rgb)
             landmarker.detect_async(mp_image, frame)
-    
+
             frame += 1
-    
+
             cv2.imshow("Landmarker", img)
             cv2.waitKey(1)
+
 
 def capture_image():
     options = vision.HandLandmarkerOptions(
         base_options=mp.tasks.BaseOptions(model_asset_path=model_path),
-        running_mode=vision.RunningMode.IMAGE
+        running_mode=vision.RunningMode.IMAGE,
     )
 
     with vision.HandLandmarker.create_from_options(options) as landmarker:
@@ -47,9 +51,15 @@ def capture_image():
         mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=img_rgb)
         result = landmarker.detect(mp_image)
 
-        print(result)
+        if result.hand_landmarks:
+            print(get_landmarks_from_data(result))
 
         cv2.imshow("Landmarker", img)
-        cv2.waitKey(100000000) # Prevents the window from closing immediately
+        cv2.waitKey(100000000)  # Prevents the window from closing immediately
+
+
+def get_landmarks_from_data(data: vision.HandLandmarkerResult):
+    return [(landmark.x, landmark.y, landmark.z) for landmark in data.hand_landmarks[0]]
+
 
 capture_image()
