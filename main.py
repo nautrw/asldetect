ac6e2c4f0fd60d802961e12490edb311e9725cf8
naut <160557714+nautrw@users.noreply.github.com>
2026-03-11 17:56:46 +0000

    remove unused capture_image function

--- main.py
+++ main.py
@@ -139,32 +139,6 @@ def capture_live_stream():
             # cv2.waitKey(1)
 
 
-def capture_image():
-    options = vision.HandLandmarkerOptions(
-        base_options=mp.tasks.BaseOptions(model_asset_path=model_path),
-        running_mode=vision.RunningMode.IMAGE,
-    )
-
-    with vision.HandLandmarker.create_from_options(options) as landmarker:
-        success, img = capture.read()
-        img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-
-        mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=img_rgb)
-        result = landmarker.detect(mp_image)
-
-        if result.hand_landmarks:
-            for hand in result.hand_landmarks:
-                print(get_clean_landmarks(hand))
-                drawing_utils.draw_landmarks(
-                    img,
-                    hand,
-                    vision.HandLandmarksConnections.HAND_CONNECTIONS,
-                )
-
-        cv2.imshow("Landmarker", img)
-        cv2.waitKey(0)
-
-
 def get_clean_landmarks(hand):
     res = []
 
